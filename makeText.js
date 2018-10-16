06ebc6bba4c7a981c8c5cd1335df6aeb912ac3e3
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-10-16 16:58:26 -0700

    Finished markov machine.

--- makeText.js
+++ makeText.js
@@ -1,2 +1,38 @@
 /** Command-line tool to generate Markov text. */
 
+const process = require('process');
+const fs = require('fs');
+const axios = require('axios');
+const MM = require('./markov');
+let mm;
+
+//arguments
+if (process.argv.length === 4) {
+  if (process.argv[2] === 'file') {
+    fs.readFile(process.argv[3], 'utf8', function(err, data) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        mm = new MM.MarkovMachine(data);
+        return data;
+      }
+    });
+  }
+
+  if (process.argv[2] === 'url') {
+    let mm = readURL(process.argv[3]);
+  }
+}
+
+async function readURL(URL) {
+  let resp;
+  try {
+    resp = await axios.get(URL);
+  } catch (e) {
+    console.error(e.message);
+    return;
+  }
+  mm = new MM.MarkovMachine(resp.data);
+  console.log(mm.makeText(500));
+  return mm;
+}
--- markov.js
+++ markov.js
@@ -1,13 +1,12 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter(c => c !== '');
+
     this.makeChains();
   }
 
@@ -17,13 +16,44 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+    for (let i = 0; i < this.words.length; i++) {
+      if (chains.get(this.words[i]) === undefined) {
+        chains.set(this.words[i], [this.words[i + 1]]);
+      } else {
+        chains.get(this.words[i]).push(this.words[i + 1]);
+      }
+    }
+    this.chains = chains;
   }
-
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let wordCount = 0;
+    let textArr = [];
+    let key = this.words[Math.floor(Math.random() * this.words.length)];
+    textArr.push(key);
+
+    while (wordCount <= numWords) {
+      if (key === undefined) {
+        break;
+      }
+      key = this.chains.get(key)[
+        Math.floor(Math.random() * this.chains.get(key).length)
+      ];
+      textArr.push(key);
+      wordCount++;
+    }
+    let str = textArr.join(' ') + '.';
+    return str;
   }
 }
+
+exports.MarkovMachine = MarkovMachine;
+
+// let mm = new MarkovMachine(
+//   'The constructor function contains some code to get you started—given some input text, it splits it on spaces and linebreak characters to make a list of words. It then calls the (unimplemented) function which builds a map of chains of'
+// );
+
+// console.log(mm.makeText());
